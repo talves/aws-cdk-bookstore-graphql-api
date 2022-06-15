628e6b187318077639a88113e0cd2800c873549d
talves <tony@3alves.com>
2022-06-15 15:48:25 -0700

    add an api-key generated to last 365 days

--- lib/bookstore-graphql-api-stack.ts
+++ lib/bookstore-graphql-api-stack.ts
@@ -1,7 +1,8 @@
-import {Stack, StackProps} from 'aws-cdk-lib';
+import {Duration, Expiration, Stack, StackProps} from 'aws-cdk-lib';
 import {Construct} from "constructs";
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 import * as appsync from "@aws-cdk/aws-appsync-alpha"
+import { CfnDisk } from 'aws-cdk-lib/aws-lightsail';
 
 export class BookstoreGraphqlApiStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
@@ -10,6 +11,15 @@ export class BookstoreGraphqlApiStack extends Stack {
     const api = new appsync.GraphqlApi(this, "MyApi", {
       name: "my-book-api",
       schema: appsync.Schema.fromAsset("graphql/schema.graphql"),
+      authorizationConfig: {
+        defaultAuthorization: {
+          authorizationType: appsync.AuthorizationType.API_KEY,
+          apiKeyConfig: {
+            name: "My very own API key",
+            expires: Expiration.after(Duration.days(365)),
+          }
+        }
+      }
     });
   }
 }
