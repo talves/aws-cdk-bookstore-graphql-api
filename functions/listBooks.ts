cb91a3e4ab52c856c734ed36e96e90a8cfea9c6a
talves <tony@3alves.com>
2022-06-15 16:57:36 -0700

    fix: name --> title

--- functions/listBooks.ts
+++ functions/listBooks.ts
@@ -2,14 +2,14 @@ export const handler = async () => {
   return [
     {
       id: "tf-123",
-      name: "My Awesome Book",
+      title: "My Awesome Book",
       completed: true,
       rating: 10,
       reviews: ["The best book ever written"],
     },
     {
       id: "def-456",
-      name: "My Terrible Book",
+      title: "My Terrible Book",
       completed: true,
       rating: 3,
       reviews: ["What was this 💩!"],
