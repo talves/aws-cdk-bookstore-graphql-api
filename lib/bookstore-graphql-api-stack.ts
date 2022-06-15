e6752f93c89e0a3824559c70b41c18fedf39fa16
talves <tony@3alves.com>
2022-06-15 12:53:36 -0700

    init 🔥

--- lib/bookstore-graphql-api-stack.ts
+++ lib/bookstore-graphql-api-stack.ts
@@ -6,11 +6,6 @@ export class BookstoreGraphqlApiStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'BookstoreGraphqlApiQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const hello: String = "Hello World";
   }
 }
