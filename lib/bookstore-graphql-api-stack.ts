e2ab171184bbe615284220935e434e4aa4feacd5
talves <tony@3alves.com>
2022-06-15 15:25:24 -0700

    add appsync (graphql) api

--- lib/bookstore-graphql-api-stack.ts
+++ lib/bookstore-graphql-api-stack.ts
@@ -1,11 +1,15 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
-import { Construct } from 'constructs';
+import {Stack, StackProps} from 'aws-cdk-lib';
+import {Construct} from "constructs";
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as appsync from "@aws-cdk/aws-appsync-alpha"
 
 export class BookstoreGraphqlApiStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    const hello: String = "Hello World";
+    const api = new appsync.GraphqlApi(this, "MyApi", {
+      name: "my-book-api",
+      schema: appsync.Schema.fromAsset("graphql/schema.graphql"),
+    });
   }
 }
