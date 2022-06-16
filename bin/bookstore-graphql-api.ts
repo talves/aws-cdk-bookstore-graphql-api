ce547fe6e4a7b2e2af5d9e93dc36ebca3f96a8d4
talves <tony@3alves.com>
2022-06-16 14:36:31 -0700

    fix: us-west-2 (oregon) supports ARM64 for functions

--- bin/bookstore-graphql-api.ts
+++ bin/bookstore-graphql-api.ts
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
-import 'source-map-support/register';
-import * as cdk from 'aws-cdk-lib';
-import { BookstoreGraphqlApiStack } from '../lib/bookstore-graphql-api-stack';
+import "source-map-support/register";
+import * as cdk from "aws-cdk-lib";
+import { BookstoreGraphqlApiStack } from "../lib/bookstore-graphql-api-stack";
 
 const app = new cdk.App();
-new BookstoreGraphqlApiStack(app, 'BookstoreGraphqlApiStack', {
+new BookstoreGraphqlApiStack(app, "BookstoreGraphqlApiStack", {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
@@ -15,7 +15,7 @@ new BookstoreGraphqlApiStack(app, 'BookstoreGraphqlApiStack', {
 
   /* Uncomment the next line if you know exactly what Account and Region you
    * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
+  env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: "us-west-2" },
 
   /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
 });
